a121ab322b2b9ecdeb7ef0e1fca62c31f949270d
rasmusbihllarsen <rasmus@rblarsen.dk>
2019-02-07 10:51:14 +0100

    Removed a PHP error

--- inc/savedata.php
+++ inc/savedata.php
@@ -1,10 +1,12 @@
 <?php
 	function gridhelper_save_postdata($post_id)
 	{
-		update_post_meta(
-			$post_id,
-			'gridhelper',
-			$_POST['gridhelper']
-		);
+		if( isset($_POST['gridhelper']) ){
+			update_post_meta(
+				$post_id,
+				'gridhelper',
+				$_POST['gridhelper']
+			);
+		}
 	}
 	add_action('save_post', 'gridhelper_save_postdata');
