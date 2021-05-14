f58ea1bde562bf080659e4987b46f87775f4a96f
Rasmus Larsen <hello@rasmusbihllarsen.com>
2021-05-14 10:33:13 +0200

    Update shortcode.php

--- inc/shortcode.php
+++ inc/shortcode.php
@@ -1,8 +1,14 @@
 <?php
 //[gridhelper]
 function rbl_gridhelper_shortcode( $atts ){
+	$a = shortcode_atts( array(
+		'post_id'	=> 0,
+		'mobile'	=> false,
+		'class'	=> '',
+	), $atts );
+
 	ob_start();
-	gridhelper();
+	gridhelper($a['post_id'], $a['mobile'], $a['class']);
 	$html = ob_get_clean();
 	return $html;
 }
